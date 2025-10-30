5c7d48c819d53b247e593f745cda81a17ba49947
dleonaraujo <dleonaraujo@ulasalle.edu.pe>
2025-10-30 09:33:21 -0500

    Ajustando la configuracion y librerias

--- config.py
+++ config.py
@@ -14,3 +14,7 @@ SWAGGER = {
     'title': 'API Hotel - Sistema de Reservas',
     'uiversion': 3
 }
+
+GOOGLE_CLIENT_ID = os.getenv('GOOGLE_CLIENT_ID','')
+GOOGLE_CLIENT_SECRET = os.getenv('GOOGLE_CLIENT_SECRET','')
+GOOGLE_DISCOVERY_URL = "https://accounts.google.com/well-known/openid-configuration"
