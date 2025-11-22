c55f7e367c920e9f31f91511dfe1b82a450b50e6
AldoVasquez07 <avasquezl@ulasalle.edu.pe>
2025-11-21 22:25:29 -0500

    ajustando index.py

--- api/index.py
+++ api/index.py
@@ -1,7 +1,4 @@
 from app import app
-from werkzeug.wsgi import DispatcherMiddleware
-from werkzeug.serving import run_wsgi_app
 
-def handler(request, response):
-    # Convierte el request serverless en WSGI
-    return run_wsgi_app(app, environ=request.environ, start_response=response)
+# Vercel usa esta variable como entrypoint
+application = app
