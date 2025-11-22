3a24b75610cb2b359611dafe773abad7a60a38d1
AldoVasquez07 <avasquezl@ulasalle.edu.pe>
2025-11-21 22:39:09 -0500

    ajustando index.py

--- app.py
+++ app.py
@@ -19,32 +19,37 @@ from routes.habitaciones_routes import habitaciones_bp
 from routes.reservas_routes import reservas_bp
 from routes.reportes_routes import reportes_bp
 
-import os
-os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
-
 
 def create_app():
     app = Flask(__name__)
+
+    # Config DB
     app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = SQLALCHEMY_TRACK_MODIFICATIONS
+
+    # Swagger
     app.config['SWAGGER'] = SWAGGER
 
     # JWT
     app.config['JWT_SECRET_KEY'] = '123456'
     app.config['JWT_ACCESS_TOKEN_EXPIRES'] = 86400
 
-    # OAuth Google
+    # Google OAuth
     app.config['GOOGLE_CLIENT_ID'] = GOOGLE_CLIENT_ID
     app.config['GOOGLE_CLIENT_SECRET'] = GOOGLE_CLIENT_SECRET
     app.config['GOOGLE_DISCOVERY_URL'] = GOOGLE_DISCOVERY_URL
 
-    # Inicializar extensiones
+    # Extensiones
     Swagger(app)
     db.init_app(app)
     Migrate(app, db)
     JWTManager(app)
 
-    init_google_client(app)
+    # Google OAuth init (protegido)
+    try:
+        init_google_client(app)
+    except Exception as e:
+        print("⚠ Error inicializando Google OAuth:", e)
 
     # Rutas
     app.register_blueprint(auth_bp)
@@ -57,7 +62,7 @@ def create_app():
     def home():
         return jsonify({
             "ok": True,
-            "message": "API Hotel running on Vercel. Visita /apidocs para Swagger UI"
+            "message": "API Hotel running on Render. Visita /apidocs para Swagger UI"
         })
 
     return app
